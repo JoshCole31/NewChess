67e5f660d5018ded9b6cf2d49fab11fb0e41fc7a
JoshCole31 <jcole@student.neumont.edu>
2023-01-09 21:58:23 -0700

    chess 960
    can start game with 960 rules

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 public class GameManager : MonoBehaviour {
 	
@@ -36,7 +37,7 @@ public class GameManager : MonoBehaviour {
 	void Start()
 	{
 		CreateBoard();
-		AddPieces();
+		//AddPieces960();
 	}
 	
 	// Create the board by placing cubes
@@ -59,9 +60,132 @@ public class GameManager : MonoBehaviour {
 			}
 		}
 	}
+    public void AddPieces960()
+    {
+        int _linePosY = 0;
+        int _piecePlayer = 1;
+        int[] spots = new int[7];
+
+        // Create all pawn at once
+        for (int i = 0; i < _boardSize; i++)
+        {
+            CreatePiece("Pawn", i, 1, 1); // Create all white pawn 	
+            CreatePiece("Pawn", i, 6, -1); // Create all dark pawn
+        }
+        //dldldldl y p 0-7
+		//place king first
+        int K1_spot = Random.Range(1, 7);
+        CreatePiece("King", K1_spot, _linePosY, _piecePlayer);
+        CreatePiece("King", K1_spot, 7, -1);
+
+        //place rooks 
+        int R1_spot = Random.Range(0, K1_spot);
+        int R2_spot = Random.Range(K1_spot+1, 8);
+
+        CreatePiece("Rook", R1_spot, _linePosY, _piecePlayer);
+        CreatePiece("Rook", R2_spot, _linePosY, _piecePlayer);
+        CreatePiece("Rook", R1_spot, 7, -1);
+        CreatePiece("Rook", R2_spot, 7, -1);
+		spots[0] = K1_spot;
+		spots[1] = R1_spot;
+		spots[2] = R2_spot;
+
+
+		bool valid = false;
+		int B1_spot = new int();
+        //places W Bishop  on a black place
+        do
+		{ 
+			B1_spot = Random.Range(0, 4);//0123
+			if (!spots.Contains<int>(value: B1_spot*2))//0246
+			{
+				valid = true;
+			}
+		} while (valid == false);
+
+        //light 
+        CreatePiece("Bishop", B1_spot * 2, _linePosY, _piecePlayer);
+        //dark
+        CreatePiece("Bishop", B1_spot * 2, 7, -1);
+
+		spots[3] = B1_spot*2;
+
+        //place second bishop but on oposit color
+        int B2_spot = new int();
+		valid= false;
+        
+		do
+		{
+            B2_spot = Random.Range(0, 8);
+			if(!spots.Contains<int>(value: B2_spot)&& B2_spot== 1|| B2_spot == 3 || B2_spot == 5 ||B2_spot ==7)
+			{
+				valid = true;
+			}
+
+		}while(valid == false);
 	
-	// Add all pieces are their respective position
-	void AddPieces()
+        CreatePiece("Bishop", B2_spot , _linePosY, _piecePlayer);
+        CreatePiece("Bishop", B2_spot , 7, -1);
+        spots[4] = B2_spot;
+
+		valid= false;
+        int Kn1_spot=new int();
+		int Kn2_spot=new int();
+		//find one open spot 
+        do
+        {
+            Kn1_spot = Random.Range(0, 8);
+            if (!spots.Contains<int>(value: Kn1_spot) )
+            {
+                valid = true;
+            }
+        } while (valid == false);
+		valid= false;
+		spots[5] = Kn1_spot;
+		//find second open spot 
+		do
+        {
+            Kn2_spot = Random.Range(0, 8);
+            if (!spots.Contains<int>(value: Kn2_spot) )
+            {
+                valid = true;
+            }
+        } while (valid == false);
+		valid=	false;
+        spots[6] = Kn2_spot;
+		//place knights
+        CreatePiece("Knight", Kn1_spot, _linePosY, _piecePlayer);
+        CreatePiece("Knight", Kn2_spot, _linePosY, _piecePlayer);
+        CreatePiece("Knight", Kn1_spot, 7, -1);
+        CreatePiece("Knight", Kn2_spot, 7, -1);
+
+		//get the last sopt for the queen but checking the array
+		int Q_spot =new int();
+        do
+        {
+            Q_spot = Random.Range(0, 8);
+            if (!spots.Contains<int>(value: Q_spot))
+            {
+                valid = true;
+            }
+        } while (valid == false);
+
+        CreatePiece("Queen", Q_spot,  _linePosY, _piecePlayer);
+        CreatePiece("Queen", Q_spot,  7, -1);
+
+    }
+
+   
+    /*//test 
+	 * Test_rookking(){
+	 * roock1 p <king 
+	 * rook2 p >king 
+	 * 
+	}
+	//*/
+    // Add all pieces are their respective position
+
+    public void AddPieces()
 	{
 		int _linePosY;
 		int _piecePlayer;
@@ -79,7 +203,7 @@ public class GameManager : MonoBehaviour {
 		CreatePiece("Rook"  , 0, _linePosY, _piecePlayer);
 		CreatePiece("Knight", 1, _linePosY, _piecePlayer);
 		CreatePiece("Bishop", 2, _linePosY, _piecePlayer);
-		CreatePiece("Queen" , 3, _linePosY, _piecePlayer);
+		CreatePiece("Queen" , 3, _posY: _linePosY, _piecePlayer);
 		CreatePiece("King"  , 4, _linePosY, _piecePlayer);
 		CreatePiece("Bishop", 5, _linePosY, _piecePlayer);
 		CreatePiece("Knight", 6, _linePosY, _piecePlayer);
